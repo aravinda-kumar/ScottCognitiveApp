b29b88fdd9f104b6018cdc0e0f0a68951348ae0c
Peiwen Luo <plu18@uclive.ac.nz>
2017-06-29 10:40:31 +1200

    First change

---
+++ ScottCognitiveApp/ScottCognitiveApp/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ScottCognitiveApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        //take photo button clicked 
+        private async void ComputerVisionTestButton_OnClicked(object sender, EventArgs e)
+        {
+
+            await Navigation.PushAsync(new CognitiveServices.Views.ComputerVisionPage());
+        }
+
+        //pick photo button clicked 
+        private async void EmotionTestButton_OnClicked(object sender, EventArgs e)
+        {
+
+
+            await Navigation.PushAsync(new CognitiveServices.Views.EmotionPage());
+
+        }
+
+        //take Video button clicked 
+        private async void OcrTestButton_OnClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new CognitiveServices.Views.OcrPage());
+        }
+
+        //pick Video button clicked 
+        private async void TextAnalyticsTestButton_OnClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new CognitiveServices.Views.TextAnalyticsPage());
+        }
+
+        private async void AIRobotButton_OnClicked(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
